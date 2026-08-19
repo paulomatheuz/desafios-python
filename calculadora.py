53c10af7f3099bf5a5a52aabd5eee21d7444e71d
Paulo Matheus <paulo.okkotsu@gmail.com>
2026-08-19 09:50:20 -0400

    Update calculadora.py
    Removed unnecessary blank lines and improved formatting.

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,6 @@
 valor1 = input("digite um número ")
-
 valor2 = input("digite outro número ")
-
 operação = input("digite a operação (+, -, *, /): ")
-
 
 if operação == "+":
     resultado = float(valor1) + float(valor2)
